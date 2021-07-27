257731c3ebc7138507576d0fd05b5ae04dd33ae0
Anatoly Ostrovsky <forgetthisbox@gmail.com>
2021-07-27 21:15:31 +0300

    kek2

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     const input1 = document.querySelector('.input1');
     const input2 = document.querySelector('.input2');
-    const button = document.querySelector('button');
+    const btn = document.querySelector('button');
 
-    button.onclick = function() {
+    btn.onclick = function() {
         console.log(
             parseInt(input2.value, 10) / parseInt(input1.value, 10)
         );
